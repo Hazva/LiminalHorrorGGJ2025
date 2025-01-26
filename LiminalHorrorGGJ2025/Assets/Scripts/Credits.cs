97762884bcd326b16beb8464f9031be9da2dbd91
Daniel Yu <danielyu742@outlook.com>
2025-01-26 13:02:07 -0800

    Credits

---
+++ LiminalHorrorGGJ2025/Assets/Scripts/Credits.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI message;
+    [SerializeField] private GameObject[] credits;
+    [SerializeField] private float messageTime = 5.0f;
+    private int creditsRevealed = 1;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        StartCoroutine(ShowMessage());
+        SC_FPSController.Instance.enabled = false;
+    }
+
+    IEnumerator ShowMessage()
+    {
+        yield return new WaitForSeconds(messageTime);
+
+        credits[0].SetActive(true);
+        AudioManager.Instance.PostEvent("Stop_Ambience");
+        AkSoundEngine.PostEvent("Play_Credits", AudioManager.Instance.gameObject, (uint)AkCallbackType.AK_MusicSyncUserCue, RevealCredit, null);
+    }
+
+    void RevealCredit(object in_cookie, AkCallbackType in_type, object in_info)
+    {
+        if (in_type == AkCallbackType.AK_MusicSyncUserCue)
+        {
+            if (creditsRevealed < credits.Length)
+            {
+                credits[creditsRevealed].SetActive(true);
+                creditsRevealed++;
+            }
+        }
+    }
+}
--- LiminalHorrorGGJ2025/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ LiminalHorrorGGJ2025/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -60,6 +60,7 @@ namespace AK
 
             namespace STATE
             {
+                static const AkUniqueID CREDITS = 2201105581U;
                 static const AkUniqueID FINAL = 565529991U;
                 static const AkUniqueID INTERMEDIATE = 2188788306U;
                 static const AkUniqueID INTERMEDIATE_EMPTY = 2512648344U;
@@ -129,6 +130,8 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
         static const AkUniqueID SUBMERGABLEAUDIO = 1607929236U;
     } // namespace BUSSES
 
