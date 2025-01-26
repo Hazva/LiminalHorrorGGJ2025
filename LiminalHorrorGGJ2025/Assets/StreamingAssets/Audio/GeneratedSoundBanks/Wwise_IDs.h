3869c2df53eda487bcab78ac2bca8f23df6eca76
tononwiman <165442066+tononwiman@users.noreply.github.com>
2025-01-25 17:35:28 -0800

    create soundbank WWsie

--- LiminalHorrorGGJ2025/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ LiminalHorrorGGJ2025/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,12 +13,52 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID FLUORESCENT_LIGHT = 1180002552U;
+        static const AkUniqueID PLAY_AMBIENT_MEADOW = 1255756422U;
+        static const AkUniqueID PLAY_AMBIENT_TUNNEL = 1637864059U;
+        static const AkUniqueID PLAY_AMBIENT_WATER_STILL = 4109800757U;
+        static const AkUniqueID PLAY_COMPASS_PICKUP = 1518103445U;
+        static const AkUniqueID PLAY_COMPASS_SPIN = 1046130049U;
+        static const AkUniqueID PLAY_CREATURE = 2891509749U;
+        static const AkUniqueID PLAY_DISTANTSUBWAY = 742699890U;
+        static const AkUniqueID PLAY_DOOR_LOOP = 2463718247U;
+        static const AkUniqueID PLAY_FLUORESCENT_LIGHT = 3799125421U;
+        static const AkUniqueID PLAY_GENERAL_FOOTSTEP_CONTAINER = 2589019387U;
+        static const AkUniqueID PLAY_HEARTBEAT = 3765695918U;
+        static const AkUniqueID PLAY_JUMPSCARE = 3840943850U;
+        static const AkUniqueID PLAY_PIPEAMBIENCE = 2778640434U;
+        static const AkUniqueID PLAY_SFX_BOTTLE_OPEN = 605122265U;
+        static const AkUniqueID PLAY_SFX_BOTTLE_PICK_UP = 2888013566U;
+        static const AkUniqueID PLAY_SWIM = 165772206U;
+        static const AkUniqueID PLAY_TRAIN_STATION_SONG = 3565446547U;
+        static const AkUniqueID PLAY_TRANSITION = 895718239U;
+        static const AkUniqueID PLAY_WATER_RISING = 95136952U;
+        static const AkUniqueID STOP_HEARTBEAT = 3319673256U;
+        static const AkUniqueID STOP_SWIM = 127261724U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace FOOTSTEP_SURFACES
+        {
+            static const AkUniqueID GROUP = 1265478494U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CARPET = 2412606308U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID TILE = 2637588553U;
+                static const AkUniqueID TILE_WET = 3922274248U;
+                static const AkUniqueID WATER = 2654748154U;
+            } // namespace STATE
+        } // namespace FOOTSTEP_SURFACES
+
+    } // namespace STATES
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMBIENT = 77978275U;
         static const AkUniqueID MASTER = 4056684167U;
     } // namespace BANKS
 
